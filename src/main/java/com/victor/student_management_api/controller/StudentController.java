10af4ac5f768b973dabdc0626c424ae33adeea92
Victor <victormazzola2019@gmail.com>
2026-03-14 16:20:33 -0300

    feat: implement student creation endpoint

--- src/main/java/com/victor/student_management_api/controller/StudentController.java
+++ src/main/java/com/victor/student_management_api/controller/StudentController.java
@@ -1,9 +1,25 @@
 package com.victor.student_management_api.controller;
 
+import com.victor.student_management_api.dto.StudentCreateRequest;
+import com.victor.student_management_api.model.Student;
+import com.victor.student_management_api.service.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody StudentCreateRequest request) {
+        return studentService.createStudent(request);
+    }
 }
