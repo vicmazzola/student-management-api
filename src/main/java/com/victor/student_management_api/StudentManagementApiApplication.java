3b5a6761eeb44ebe42262afe4766b488e2d5d397
Victor <victormazzola2019@gmail.com>
2026-03-14 14:19:00 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/victor/student_management_api/StudentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.victor.student_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/student_management_api/StudentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.student_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
