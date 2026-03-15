d336d42ab55015af1a45317faa0e33d1b150cae3
Victor <victormazzola2019@gmail.com>
2026-03-15 17:09:11 -0300

    feat: add validation to ensure grade is between 0 and 10

--- src/main/java/com/victor/student_management_api/dto/StudentSubjectRequest.java
+++ src/main/java/com/victor/student_management_api/dto/StudentSubjectRequest.java
@@ -1,5 +1,7 @@
 package com.victor.student_management_api.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,6 +11,8 @@ public record StudentSubjectRequest(
         String subject,
 
         @NotNull
+        @Min(value = 0, message = "grade must be between 0 and 10")
+        @Max(value = 10, message = "grade must be between 0 and 10")
         Integer grade
 ) {
 }
