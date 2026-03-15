259b6e01e7ea32d6fe055dba4c8c22403d69123b
Victor <victormazzola2019@gmail.com>
2026-03-15 17:02:44 -0300

    feat: add example dataset using example-data.sql for testing

---
+++ src/main/resources/example-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_registro_aluno (matricula, nome, sexo, nascimento) VALUES
+(1, 'Erik Oliver', 'M', '1987-10-10'),
+(2, 'Rafael Dinarzio', 'M', '1984-09-27'),
+(3, 'Daniel Limis', 'M', '1982-11-28'),
+(4, 'Gustavo Zangotto', 'M', '1983-07-18'),
+(5, 'Gabriel Milhote', 'M', '1995-09-05'),
+(6, 'Bia Santos', 'F', '1998-07-08');
+
+INSERT INTO tb_aluno_materias (materia, nota_final, matricula) VALUES
+('Matemática', 10, 1),
+('Física', 9, 1),
+('Matemática', 5, 3),
+('Geografia', 6, 3),
+('Inglês', 7, 3),
+('Matemática', 10, 6),
+('Geografia', 8, 2),
+('Inglês', 10, 2);
